bb71bf371c4c4187cf61ae313cf796e0ff1a4e39
skad00sh <heramblonkar@gmail.com>
2021-06-04 07:14:39 +0530

    Initial Implementation of GetImageParameter class

--- image_param/api_wrappers/api_wrappers.py
+++ image_param/api_wrappers/api_wrappers.py
@@ -9,7 +9,10 @@ from image_param.IP_CONSTANTS.CONSTANTS import *
 
 
 class GetImage:
-    # no doc :/
+    """
+    To get a single image
+    Doc pending :/
+    """
     def __init__(self,
                  starttime: datetime,
                  aia_wave: (AIA_WAVE, list),
@@ -28,3 +31,32 @@ class GetImage:
         response = requests.get(self.prepare_url())
         img = Image.open(BytesIO(response.content))
         return img
+
+class GetImageParameter:
+    """
+    To get parameters of single image
+    Doc pending :/
+    """
+
+    def __init__(self,
+                 starttime: datetime,
+                 aia_wave: (AIA_WAVE, list),
+                 image_size: (IMAGE_SIZE, str),
+                 param_id: (IMAGE_PARAM, str)):
+        self.starttime = starttime
+        self.aia_wave = aia_wave
+        self.image_size = image_size
+        self.param_id = param_id
+
+    def prepare_url(self) -> str:
+        time_str = datetime.strftime(self.starttime, '%Y-%m-%dT%H:%M:%S')
+        # self.time_str = datetime.strftime(self.starttime, '%Y-%m-%dT%H:%M:%S')
+        prepared_url = URL_STRINGS.aia_imageparam_jpeg_url.format(self.image_size, self.aia_wave, time_str,
+                                                                  self.param_id)
+        return prepared_url
+
+    def get_image(self) -> Image:
+        response = requests.get(self.prepare_url())
+        img = Image.open(BytesIO(response.content))
+        return img
+    
